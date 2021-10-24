142a78f0e82805a299c66132bdba99f9a6eb0e4b
Shirin <sirin999b@gmail.com>
2021-10-24 18:30:31 -0400

    initial commit

---
+++ src/main/java/com/curahealthyme/project/CuraHealthyMeApplication.java
@@ -0,0 +1,13 @@
+package com.curahealthyme.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuraHealthyMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuraHealthyMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curahealthyme/project/CuraHealthyMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curahealthyme.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuraHealthyMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
